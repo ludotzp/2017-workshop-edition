97b2fe53a00214ba8de352ea14fe4ee176e36e5e
ludotzp <ludovic.touzepeiffer@gmail.com>
2017-07-19 11:41:42 -0400

    Update search.js

--- public/js/search.js
+++ public/js/search.js
@@ -37,7 +37,7 @@ var store = [{% for text in site.texts %}{
   "layout": {{ text.layout | jsonify }},
   "link": {{text.url | jsonify}},
   "mode": {{text.mode | jsonify}}, 
-  "excerpt": {{text.mode | append: " " | append: text.content | strip_html | remove: "-"| remove: "[TOC] | "| replace: 'tc', '<b>Transcription</b>'| replace: 'tl', '<b>Translation</b>'| replace: 'tcn', '<b>Normalized Transcription</b>'| truncatewords: 20 | jsonify}}
+  "excerpt": {{text.mode | append: " " | append: text.content | strip_html | remove: "[translation]" | remove: "[diplomatic]" |remove: "-"| remove: "[TOC] | "| replace: 'tcn', '<b>Normalized Transcription</b>'|replace: 'tc', '<b>Transcription</b>'| replace: 'tl', '<b>Translation</b>'| truncatewords: 20 | jsonify}}
 }
 {% unless forloop.last %},{% endunless %}{% endfor %}]
 
